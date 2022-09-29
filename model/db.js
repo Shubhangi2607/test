08e31e8b0afa1591787e970affeb1e733ae8fcf4
Samirahmadfaruki <87942888+Samirahmadfaruki@users.noreply.github.com>
2022-09-30 04:01:53 +0530

    commit

---
+++ model/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql");
+
+const connUri = {
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "ecommerce",
+  multipleStatements: true,
+};
+
+const conn = mysql.createConnection(connUri);
+conn.connect((err) => {
+  if (!err) {
+    console.log("Connection is Success!");
+  } else {
+    console.log(err);
+  }
+});
+
+module.exports = conn;
